76649bb095867ba3c5746d780459577369c5b883
wowagenciacriativa <gabriel@wowagenciacriativa.com.br>
2021-08-24 12:16:59 -0300

    Funções Processo Seletivo Escribo

---
+++ dart/main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+int sumAll(val) {
+  val = int.parse(val);
+  int sum = 0;
+
+  // Faço um loop até o valor informado
+  for (var i = 0; i < val; i++) {
+    // Verifico se o valor é dividendo por 3 ou 5
+    if ((i % 3) == 0 || (i % 5) == 0) {
+      // Caso o valor seja dividido acrescendo o valor atual do loop
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+void main() {
+  stdout.write("Digite um número inteiro: ");
+  var val = stdin.readLineSync();
+
+  stdout.write(sumAll(val));
+}
